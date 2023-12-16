a04c686b9acd22dec12adfe963ac4968f70c6c9c
Git-OT-Hub <nw7649sss@gmail.com>
2023-12-16 18:48:33 +0900

    Add: ゲストログインのコントローラー作成

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class UserSessionsController < ApplicationController
-  skip_before_action :require_login, only: %i[new create]
+  skip_before_action :require_login, only: %i[new create guest_login]
 
   def new; end
 
@@ -17,5 +17,27 @@ class UserSessionsController < ApplicationController
   def destroy
     logout
     redirect_to root_path, success: t(".success"), status: :see_other
+  end
+
+  def guest_login
+    if current_user
+      redirect_to root_path, danger: t(".already_logged_in")
+    end
+    
+    loop do
+      random_email = SecureRandom.alphanumeric(10)
+      unless User.exists?(email: "#{random_email}@example.com")
+        @guest_email = "#{random_email}@example.com"
+        break
+      end
+    end
+
+    if @guest_user = User.create(name: "ゲスト", email: @guest_email, password: "password", password_confirmation: "password")
+      auto_login(@guest_user)
+      redirect_back_or_to root_path, success: t(".guest_login_success")
+    else
+      flash.now[:danger] = t(".guest_login_fail")
+      render :new, status: :unprocessable_entity
+    end
   end
 end

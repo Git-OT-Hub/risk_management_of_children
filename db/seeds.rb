a0bb18cc54589c928728ae32dadad8736b65223f
Git-OT-Hub <nw7649sss@gmail.com>
2023-12-17 18:42:24 +0900

    Add: gem fakerの導入とseedsファイルを用いてのデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+10.times do
+  User.create!(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.unique.email,
+    password: "P@ssw0rd",
+    password_confirmation: "P@ssw0rd"
+  )
+end
+
+20.times do |n|
+  Post.create!(
+    user: User.offset(rand(User.count)).first, 
+    title: "タイトル#{n}", 
+    body: "本文#{n}"
+  )
+end

6d3537d4b899bebdecbdd17450f3d8d4cc523ad7
Git-OT-Hub <nw7649sss@gmail.com>
2024-01-21 12:03:38 +0900

    Add: タイトルを動的に出力するよう実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def turbo_stream_flash
     turbo_stream.update "flash_message", partial: "shared/flash_message"
   end
+
+  def page_title(page_title = "")
+    base_title = "INDOOR BABY GUARDIAN"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
 end

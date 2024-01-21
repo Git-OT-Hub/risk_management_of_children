1a57ea53f373192d7abce0aa957deef3b65b72dd
Git-OT-Hub <nw7649sss@gmail.com>
2024-01-21 21:44:00 +0900

    Add: お気に入り登録に関するテーブルとルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
       end
     end
   end
+  resources :bookmarks, only: %i[create destroy]
   resource :my_page, only: %i[show edit update] do
     collection do
       get "bookmarks", action: :bookmarks

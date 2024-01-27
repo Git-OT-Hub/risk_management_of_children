86a49c751a7c4b4866241fbb6f7d9c90423b3519
Git-OT-Hub <nw7649sss@gmail.com>
2024-01-27 12:08:47 +0900

    Add: いいね機能に関するコントローラの実装

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,19 @@
+class FavoritesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    current_user.favorite(@post)
+    respond_to do |format|
+      format.turbo_stream { flash.now[:success] = t(".success") }
+      format.html { redirect_back fallback_location: posts_path, success: t(".success") }
+    end
+  end
+
+  def destroy
+    @post = current_user.favorites.find(params[:id]).post
+    current_user.remove_favorite(@post)
+    respond_to do |format|
+      format.turbo_stream { flash.now[:success] = t(".success") }
+      format.html { redirect_back fallback_location: posts_path, success: t(".success") }
+    end
+  end
+end

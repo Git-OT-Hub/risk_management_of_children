89fd83e11d44640e72301c158756cac2e08cb55c
Git-OT-Hub <nw7649sss@gmail.com>
2024-01-28 18:42:34 +0900

    Add: my_postsアクションの実装

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -8,6 +8,14 @@ class MyPagesController < ApplicationController
   def update
   end
 
+  def my_posts
+    @my_posts = current_user.posts.includes([:user, :bookmarks, :favorites]).with_attached_images.order(created_at: :desc)
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.update("my_page_item_list", partial: "my_posts_list", locals: { my_posts: @my_posts }) }
+      format.html {  }
+    end
+  end
+
   def bookmarks
     @bookmark_posts = current_user.bookmark_posts.includes([:user, :bookmarks, :favorites]).with_attached_images.order(created_at: :desc)
     respond_to do |format|

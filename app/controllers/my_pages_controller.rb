079943f2fc4317d25ceb573bddec475effcb465d
Git-OT-Hub <nw7649sss@gmail.com>
2024-02-03 13:17:32 +0900

    Add: User情報の編集に関する処理をコントローラに追加

--- app/controllers/my_pages_controller.rb
+++ app/controllers/my_pages_controller.rb
@@ -1,11 +1,17 @@
 class MyPagesController < ApplicationController
-  def show
-  end
+  before_action :set_user, only: %i[edit update]
 
-  def edit
-  end
+  def show; end
+
+  def edit; end
 
   def update
+    if @user.update(user_update_params)
+      redirect_to my_page_path, success: t("defaults.message.updated", item: User.model_name.human)
+    else
+      flash.now[:danger] = t("defaults.message.not_updated", item: User.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def my_posts
@@ -16,5 +22,15 @@ class MyPagesController < ApplicationController
   def bookmarks
     @q = current_user.bookmark_posts.ransack(params[:q])
     @bookmark_posts = @q.result(distinct: true).includes([:user, :bookmarks, :favorites]).with_attached_images.order(created_at: :desc).page(params[:page]).per(6)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_update_params
+    params.require(:user).permit(:name, :email, :avatar)
   end
 end

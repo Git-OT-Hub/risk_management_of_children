4d14d7132355203a26987281f187ef5720489b61
Git-OT-Hub <nw7649sss@gmail.com>
2024-02-10 12:23:38 +0900

    Add: バリデーションの実装

--- app/models/diagnosis_content.rb
+++ app/models/diagnosis_content.rb
@@ -1,2 +1,8 @@
 class DiagnosisContent < ApplicationRecord
+  validates :number, presence: true, uniqueness: true
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :danger, length: { maximum: 65_535 }
+  validates :recommend, length: { maximum: 65_535 }
+  validates :rakuten_item_url, length: { maximum: 255 }
+  validates :rakuten_item_image_url, length: { maximum: 255 }
 end

e1f508cd751c4eeab91a49ccb3a8eadef4a77231
Git-OT-Hub <nw7649sss@gmail.com>
2024-02-13 00:13:02 +0900

    Add: コンテンツの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ DiagnosisContent.create!(
   title: "角や縁がむき出しの家具",
   image: "diagnosis_content_image_1.png",
   danger: "走り回ったり転んだりした拍子に、家具の角や縁にぶつかって、思わぬ怪我につながる恐れがあります。",
-  item_name: "コーナーガード（ベビーガード）",
+  item_name: "コーナーガード（コーナークッション）",
   item_description: "家具やテーブルの角や縁に設置することで、とがった部分をカバーするグッズ。",
   item_point: "コーナーガードは、L型・U型・波型などの形状があります。それぞれ適した使用箇所があるため、家具に合わせて使い分けるとよいでしょう。\r\n\r\nまた、インテリアと合わない色を使用すると、コーナーガード部分が目立ってしまい、子どもの興味を惹いてしまう可能性があるため、インテリアの色に合ったものを選ぶのがおすすめです。",
   rakuten_item_image_urls: ["https://hbb.afl.rakuten.co.jp/hgb/3946afe2.64e8d877.3946afe3.d59854b8/?me_id=1367305&item_id=10000042&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fplaisiureux%2Fcabinet%2F09353293%2F09723035%2Fimgrc0161350196.jpg%3F_ex%3D300x300&s=300x300&t=pict", "https://hbb.afl.rakuten.co.jp/hgb/39556432.c4b74e74.39556433.ef7bb90c/?me_id=1411285&item_id=10059283&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fgeki-pla%2Fcabinet%2Fitempic2290%2Fzak-32802.jpg%3F_ex%3D300x300&s=300x300&t=pict", "https://hbb.afl.rakuten.co.jp/hgb/39556bf7.7da170ea.39556bf8.d124260e/?me_id=1344749&item_id=10000267&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Ftonarinoomise%2Fcabinet%2Fimgrc0084532965.jpg%3F_ex%3D300x300&s=300x300&t=pict"],
@@ -42,10 +42,36 @@ DiagnosisContent.create!(
   title: "子どもの手が届く施錠のない引き出しや開き戸収納など",
   image: "diagnosis_content_image_4.png",
   danger: "引き出しや開き戸収納などで、指をはさんで怪我をする恐れがあります。\r\n\r\nまた、刃物や洗剤など、子どもにとって危険な物が収納されている場合、怪我につながる恐れがあります。",
-  item_name: "チャイルドロック（ベビーガード、ドアストッパー）",
+  item_name: "チャイルドロック（ドアロック）",
   item_description: "引き出しや開き戸収納などに取り付けることで、鍵の役割をはたし、中の物の誤飲や指をはさむといった危険から子どもを守るグッズ。",
   item_point: "チャイルドロックには、バンド式や回転式、オートロック式など、いくつか種類があるため、取り付け場所の状況（引き出しの厚みや、段差の有無、取り付け面の広さなど）を事前に調査することが重要です。\r\n\r\nバンド式は、2箇所の固定部分をベルトのようにつなぎ、また、バンドの長さを調整できる物もあるため、ある程度の場所に対応可能です。\r\n\r\nチャイルドロックは、家具の外側に取り付けるため、家具と全く違う色を使用すると、子どもの興味をひいてしまう可能性があります。家具の色に合った目立たない物を選ぶのがおすすめです。",
   rakuten_item_image_urls: ["https://hbb.afl.rakuten.co.jp/hgb/3951377e.c455e6a3.3951377f.81477c49/?me_id=1387760&item_id=10000022&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fhigh-high%2Fcabinet%2Fproduct%2Ftop%2Fbaby-guard-001-r.jpg%3F_ex%3D300x300&s=300x300&t=pict", "https://hbb.afl.rakuten.co.jp/hgb/3951377e.c455e6a3.3951377f.81477c49/?me_id=1387760&item_id=10000418&pc=https%3A%2F%2Fimage.rakuten.co.jp%2Fhigh-high%2Fcabinet%2Fproduct%2F07363483%2F09406652%2F02.jpg%3F_ex%3D300x300&s=300x300&t=pict", "https://hbb.afl.rakuten.co.jp/hgb/39566d63.9e5fe9a0.39566d65.d677ad3e/?me_id=1388478&item_id=10000142&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Ffelicitations%2Fcabinet%2Fchild_lock%2Fimgrc0100726857.jpg%3F_ex%3D300x300&s=300x300&t=pict"],
   rakuten_item_urls: ["https://hb.afl.rakuten.co.jp/ichiba/3951377e.c455e6a3.3951377f.81477c49/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fhigh-high%2Fbaby-guard-001%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjMwMHgzMDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MSwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9", "https://hb.afl.rakuten.co.jp/ichiba/3951377e.c455e6a3.3951377f.81477c49/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fhigh-high%2Fdoorlock02%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjMwMHgzMDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9", "https://hb.afl.rakuten.co.jp/ichiba/39566d63.9e5fe9a0.39566d65.d677ad3e/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffelicitations%2F20000016%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjMwMHgzMDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9"],
   rakuten_item_names: ["1位受賞【LINE追加で5％OFF】 HUGmeee チャイルドロック 引き出し 冷蔵庫 ベビーガード ストッパー ドア ドアロック 赤ちゃん 棚 タンス 扉 ダイヤル式 鍵 いたずら防止 6個入り ワンタッチ 指挟み防止", "【LINE追加で5％OFF】 チャイルドロック 扉 引き戸 引き出し 冷蔵庫 開き戸 観音開き 観音扉 両開き 戸棚 ドアストッパー 赤ちゃん 戸口ロック ベビー 5個セット ストッパー セーフティーグッズ 地震 戸棚 開き戸をロック", "チャイルドロック マグネット 12個 チャイルドロック 引き出し ドア ベビーガード チャイルドロック 扉"]
+)
+
+DiagnosisContent.create!(
+  number: 5,
+  title: "子どもの行動範囲内に設置されているドア（開き戸・引き戸）",
+  image: "diagnosis_content_image_5.png",
+  danger: "開き戸や引き戸などの開け閉めの際に、指をはさんで怪我をする恐れがあります（開き戸の場合は、戸の付け根の丁番側でも、指をはさんで怪我をする恐れがあります）。",
+  item_name: "ハンドル、引手側（ドアクッション、ドアストッパー）  丁番側（フィンガーアラート、指はさみ防止カバー）",
+  item_description: "【ハンドル、引手側（ドアクッション、ドアストッパー）】\r\n開き戸や引き戸に設置することで、ドアが閉まる時に、ハンドル、引手側で子どもの指や手をはさむ事故を防ぐグッズ。\r\n\r\n【丁番側（フィンガーアラート、指はさみ防止カバー）】\r\n開き戸の開閉時に、丁番側で子どもの指や手をはさむ事故を防ぐグッズ。",
+  item_point: "【ハンドル、引手側（ドアクッション、ドアストッパー）】\r\nドアの側面や上面にはさみ込んで使うタイプのドアクッションを選ぶ際は、設置するドアの厚さに対応できるかどうかを確認する必要があります。そして、子どもが立ち上がって手を伸ばしても、届かない高さに取り付けましょう。\r\n\r\n【丁番側（フィンガーアラート、指はさみ防止カバー）】\r\nフィンガーアラートを選ぶ際は、サイズに注意しましょう。子どもが立ち上がって手を伸ばしても、指や手が守られる高さの物を選びましょう。\r\nまた、子どもは成長するのが早く、身長が伸びて買い替えなければならないことを考えると、最初から高さがある物を選択するのもおすすめです。\r\n\r\nドアの両面に取り付ける必要があるため、内側カバー、外側カバーの両方を購入して設置しましょう。",
+  rakuten_item_image_urls: ["https://hbb.afl.rakuten.co.jp/hgb/39584910.bd93729f.39584911.3605ab15/?me_id=1308652&item_id=10004964&pc=https%3A%2F%2Fimage.rakuten.co.jp%2Ftorekagu%2Fcabinet%2Fcompass1665546177.jpg%3F_ex%3D300x300&s=300x300&t=pict", "https://hbb.afl.rakuten.co.jp/hgb/395861a1.47c5f453.395861a2.15e1c004/?me_id=1332199&item_id=10000001&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Ffingeralert%2Fcabinet%2Fmem_item3%2Fset_1800.jpg%3F_ex%3D300x300&s=300x300&t=pict"],
+  rakuten_item_urls: ["https://hb.afl.rakuten.co.jp/ichiba/39584910.bd93729f.39584911.3605ab15/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ftorekagu%2F4589971642618%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjMwMHgzMDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9", "https://hb.afl.rakuten.co.jp/ichiba/395861a1.47c5f453.395861a2.15e1c004/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffingeralert%2F10000002%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjMwMHgzMDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9"],
+  rakuten_item_names: ["ドアストッパー 3個セット ドアクッション ドアストップ 換気 柔らかい 便利 安全 玄関 ベビー キッズ ペット 室内 セーフティ ポイント消化", "指はさみ防止 ドア 子供 フィンガーアラート 蝶番 1800mm 内・外側カバーセット 指詰め防止 ドア挟み防止 ストッパー ストップ セーフティ キッズ ベビー 赤ちゃん こども お子さま 自宅 施設 保育園 幼児園 幼稚園 病院 工場 小学校 店舗"]
+)
+
+DiagnosisContent.create!(
+  number: 6,
+  title: "子どもの行動範囲内に設置されている網戸",
+  image: "diagnosis_content_image_6.png",
+  danger: "窓を網戸にして換気をする場合、網戸は軽いため子どもの力だけで開け閉めができてしまいます。そのため、子どもが網戸を開けた際に、外へ転落してしまう恐れがあります。\r\n\r\nまた、網戸を開かなくしただけでは、窓は動いてしまうため、窓で指をはさむ危険性があります。",
+  item_name: "網戸（網戸ストッパー、網戸ロック）  窓（窓ストッパー）",
+  item_description: "【網戸（網戸ストッパー、網戸ロック）】\r\n網戸の上部に設置することで、ロック時は網戸が開かなくなり、子どもが外へ転落する事故を防ぐグッズ。\r\n\r\n【窓（窓ストッパー）】\r\n窓のレール上などに設置することで、窓を固定することができ、子どもが窓で指をはさむ事故を防ぐグッズ。",
+  item_point: "【網戸（網戸ストッパー、網戸ロック）】\r\n網戸ストッパーを設置する際は、子どもの手が届かない網戸の上部に設置するようにしましょう。また、設置する位置が適切でない場合、網戸ストッパーをロック状態にしていても、網戸がある程度開いてしまうため、設置する位置は注意しましょう。\r\n\r\n【窓（窓ストッパー）】\r\n網戸ストッパーを設置して、網戸を開けなくしても、窓は動くため、子どもが窓で指をはさむ危険性が残ってしまいます。そのため、窓ストッパーを設置して、窓も固定する必要があります。\r\nこちらも、設置する位置が適切でない場合、窓がある程度開いてしまうため、設置する位置は注意しましょう。",
+  rakuten_item_image_urls: ["https://hbb.afl.rakuten.co.jp/hgb/395a1c5d.96c56ce0.395a1c5e.3de15788/?me_id=1402408&item_id=10000027&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fbabykids-hrt%2Fcabinet%2Fh001-050%2Fh011_01_v2.jpg%3F_ex%3D300x300&s=300x300&t=pict", "https://hbb.afl.rakuten.co.jp/hgb/395a2b25.8729d7e6.395a2b26.b8d6192c/?me_id=1347924&item_id=10042619&pc=https%3A%2F%2Fimage.rakuten.co.jp%2Fe-kit%2Fcabinet%2Fekit%2Fek-mb001_2.jpg%3F_ex%3D300x300&s=300x300&t=pict"],
+  rakuten_item_urls: ["https://hb.afl.rakuten.co.jp/ichiba/395a1c5d.96c56ce0.395a1c5e.3de15788/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbabykids-hrt%2Fh011%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjMwMHgzMDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9", "https://hb.afl.rakuten.co.jp/ichiba/395a2b25.8729d7e6.395a2b26.b8d6192c/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fe-kit%2Fek-mb001%2F&link_type=hybrid_url&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJoeWJyaWRfdXJsIiwic2l6ZSI6IjMwMHgzMDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9"],
+  rakuten_item_names: ["網戸ロック │ ワンタッチ しまり 網戸ストッパー サッシロック サッシストッパー 網戸 サッシ ロック ストッパー 換気 防犯 防犯グッズ いたずら防止 簡易鍵 鍵 簡易錠 補助錠 取り付け 簡単 安全 ベランダ チャイルドロック 赤ちゃん ペット 猫 窓 子供 子ども こども", "安心3個組 [CICADA] 鍵付き 窓 ロック 窓ロック 子供 ストッパー 換気 サッシロック 防犯 防犯グッズ 引き戸 サッシ 赤ちゃん 転落防止 チャイルドロック ペット 脱走 安全 補助鍵 取り付け 簡単 窓用セーフティーロック [MADO BLOCK]"]
 )

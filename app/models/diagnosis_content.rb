5c20de9837cac110882aaec797c78e4b88791d35
Git-OT-Hub <nw7649sss@gmail.com>
2024-02-12 14:07:44 +0900

    Add: バリデーションの追加及びrakuten_item_namesカラムが配列を扱えるよう定義

--- app/models/diagnosis_content.rb
+++ app/models/diagnosis_content.rb
@@ -1,6 +1,7 @@
 class DiagnosisContent < ApplicationRecord
   serialize :rakuten_item_image_urls, type: Array, coder: YAML
   serialize :rakuten_item_urls, type: Array, coder: YAML
+  serialize :rakuten_item_names, type: Array, coder: YAML
 
   validates :number, presence: true, uniqueness: true
   validates :title, presence: true, length: { maximum: 255 }
@@ -9,7 +10,7 @@ class DiagnosisContent < ApplicationRecord
   validates :item_name, length: { maximum: 255 }
   validates :item_description, length: { maximum: 65_535 }
   validates :item_point, length: { maximum: 65_535 }
-  validate :validate_rakuten_item_image_urls, :validate_rakuten_item_urls
+  validate :validate_rakuten_item_image_urls, :validate_rakuten_item_urls, :validate_rakuten_item_names
 
   private
 
@@ -24,6 +25,10 @@ class DiagnosisContent < ApplicationRecord
       errors.add(:rakuten_item_urls, ": 65_535文字以内にしてください。") if url.length > 65_535
     end
   end
-end
 
-rakuten_item_names
+  def validate_rakuten_item_names
+    rakuten_item_names.each do |name|
+      errors.add(:rakuten_item_names, ": 65_535文字以内にしてください。") if name.length > 65_535
+    end
+  end
+end

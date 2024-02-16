17d17fea96dae46c0b1d0402b4e30c76ce77ad12
Git-OT-Hub <nw7649sss@gmail.com>
2024-02-16 21:38:30 +0900

    Add: バリデーションの追加及ぼ、rakuten_item_tagsカラムが配列を扱えるよう定義

--- app/models/diagnosis_content.rb
+++ app/models/diagnosis_content.rb
@@ -2,6 +2,7 @@ class DiagnosisContent < ApplicationRecord
   serialize :rakuten_item_image_urls, type: Array, coder: YAML
   serialize :rakuten_item_urls, type: Array, coder: YAML
   serialize :rakuten_item_names, type: Array, coder: YAML
+  serialize :rakuten_item_tags, type: Array, coder: YAML
 
   validates :number, presence: true, uniqueness: true
   validates :title, presence: true, length: { maximum: 255 }
@@ -29,6 +30,12 @@ class DiagnosisContent < ApplicationRecord
   def validate_rakuten_item_names
     rakuten_item_names.each do |name|
       errors.add(:rakuten_item_names, ": 65_535文字以内にしてください。") if name.length > 65_535
+    end
+  end
+
+  def validate_rakuten_item_tags
+    rakuten_item_tags.each do |tag|
+      errors.add(:rakuten_item_tags, ": 65_535文字以内にしてください。") if tag.length > 65_535
     end
   end
 end

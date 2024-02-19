096742345bf34bec1aa420b17487a076b6985f3c
Git-OT-Hub <nw7649sss@gmail.com>
2024-02-19 23:46:14 +0900

    Add: 質問画面におけるデザイン修正及び、jsを用いてのステップ入力実装

--- app/javascript/controllers/diagnosis_questions_controller.js
+++ app/javascript/controllers/diagnosis_questions_controller.js
@@ -1,8 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = []
-
+  
+  static targets = ["start_diagnosis", "previous_button", "next_button", "submit_button", ...Array.from({length: 15}, (_, i) => `question_${i + 1}`)];
+    
   connect() {
+  }
+
+  currentQuestion = 1;
+
+  startDiagnosis() {
+    this.start_diagnosisTarget.classList.add("visually-hidden");
+    this[`question_${this.currentQuestion}Target`].classList.remove("visually-hidden");
+    this.updateButtonVisibility();
+  }
+
+  updateButtonVisibility() {
+    this.previous_buttonTarget.classList.toggle("visually-hidden", this.currentQuestion <= 1);
+    this.next_buttonTarget.classList.toggle("visually-hidden", this.currentQuestion >= 15);
+    this.submit_buttonTarget.classList.toggle("visually-hidden", this.currentQuestion !== 15);
   }
 }

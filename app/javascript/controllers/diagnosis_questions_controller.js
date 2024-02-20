74b47b72eef3b12fd4efbb6502c1a377d1a23831
Git-OT-Hub <nw7649sss@gmail.com>
2024-02-20 23:55:29 +0900

    Add: 質問機能における、jsを使用してのステップ入力の実装

--- app/javascript/controllers/diagnosis_questions_controller.js
+++ app/javascript/controllers/diagnosis_questions_controller.js
@@ -2,12 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   
-  static targets = ["start_diagnosis", "previous_button", "next_button", "submit_button", ...Array.from({length: 15}, (_, i) => `question_${i + 1}`)];
+  static targets = ["start_diagnosis", "previous_button", "next_button", "submit_button", "all_questions", ...Array.from({length: 15}, (_, i) => `question_${i + 1}`)];
     
-  connect() {
-  }
-
   currentQuestion = 1;
+
+  connect() {
+    this.addRadioListeners();
+  }
 
   startDiagnosis() {
     this.start_diagnosisTarget.classList.add("visually-hidden");
@@ -19,5 +20,27 @@ export default class extends Controller {
     this.previous_buttonTarget.classList.toggle("visually-hidden", this.currentQuestion <= 1);
     this.next_buttonTarget.classList.toggle("visually-hidden", this.currentQuestion >= 15);
     this.submit_buttonTarget.classList.toggle("visually-hidden", this.currentQuestion !== 15);
+  }
+
+  addRadioListeners() {
+    this.all_questionsTargets.forEach((question) => {
+      const radioButtons = question.querySelectorAll('input[type="radio"]');
+      radioButtons.forEach((radio) => {
+        radio.addEventListener('change', () => {
+          this.updateActiveLabels(question, radio);
+        });
+      });
+    });
+  }
+
+  updateActiveLabels(question, selectedRadio) {
+    const labels = question.querySelectorAll('label');
+    labels.forEach((label) => {
+      if (label.getAttribute('for') === selectedRadio.getAttribute('id')) {
+        label.classList.add('active');
+      } else {
+        label.classList.remove('active');
+      }
+    });
   }
 }

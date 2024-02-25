a2d4d780bcf942dc710b8580978d91060060cfc0
Git-OT-Hub <nw7649sss@gmail.com>
2024-02-25 21:26:17 +0900

    Add: 診断結果保存モデルにバリデーションを実装

--- app/models/diagnosis_result.rb
+++ app/models/diagnosis_result.rb
@@ -1,3 +1,20 @@
 class DiagnosisResult < ApplicationRecord
   belongs_to :user
+
+  validates :title, length: { maximum: 255 }
+  validate :validate_results, :validate_statuses
+
+  private
+
+  def validate_results
+    results.each do |result|
+      errors.add(:results, ": 65_535文字以内にしてください。") if result.length > 65_535
+    end
+  end
+
+  def validate_statuses
+    statuses.each do |status|
+      errors.add(:statuses, ": 65_535文字以内にしてください。") if status.length > 65_535
+    end
+  end
 end

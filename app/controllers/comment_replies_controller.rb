927ed8695c60e435ddd45f615d718c501172bfc5
Git-OT-Hub <nw7649sss@gmail.com>
2024-03-09 16:32:30 +0900

    Add: コメント返信機能における一覧表示機能の実装

--- app/controllers/comment_replies_controller.rb
+++ app/controllers/comment_replies_controller.rb
@@ -1,9 +1,19 @@
 class CommentRepliesController < ApplicationController
   skip_before_action :require_login, only: %i[index]
+  before_action :set_comment, only: %i[index new]
   #before_action :set_comment_reply, only: %i[edit update destroy]
 
   def index
-    @comment = Comment.find(params[:comment_id])
+    @comment_replies = @comment.comment_replies.all
+  end
+
+  def new
     #binding.pry
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:comment_id])
   end
 end

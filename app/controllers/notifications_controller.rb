a1298ffa136568cb240cfcd0ae10e285eef0c9df
Git-OT-Hub <nw7649sss@gmail.com>
2024-03-18 21:22:05 +0900

    Add: 通知機能における個別の既読処理機能の実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,24 @@
 class NotificationsController < ApplicationController
 
-  def index
-  end
-
   def update
+    @notification = current_user.received_notifications.find(params[:id])
+    @notification.update(read: true)
+    if @notification
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.remove(@notification),
+            turbo_stream.replace("notification_count", partial: "notifications/notification_count")
+          ]
+        end
+        format.html {  }
+      end
+    else
+      redirect_to root_path, danger: t("notifications.index.update_fail")
+    end
   end
 
   def mark_all_as_read
+    #kokokara update_all処理に変更
   end
 end

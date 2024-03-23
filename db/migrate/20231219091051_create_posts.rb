43a2a0b7439c7f6e0b5960adbf69fb47e13e47eb
Git-OT-Hub <nw7649sss@gmail.com>
2024-03-23 15:08:41 +0900

    Fix: postsテーブルの修正

--- db/migrate/20231219091051_create_posts.rb
+++ db/migrate/20231219091051_create_posts.rb
@@ -3,6 +3,10 @@ class CreatePosts < ActiveRecord::Migration[7.1]
     create_table :posts do |t|
       t.string :title, null: false
       t.text :body
+      t.text :item_info
+      t.integer :item_category, default: 0
+      t.text :item_merit
+      t.text :item_demerit
       t.references :user, foreign_key: true
       t.integer :favorites_count, default: 0
       t.integer :comments_count, default: 0

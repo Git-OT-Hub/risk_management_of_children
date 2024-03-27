baccbd2a3586aa8bc3307d5d2a6f74b362e3b47a
Git-OT-Hub <nw7649sss@gmail.com>
2024-03-27 23:08:02 +0900

    Fix: 通知一覧のフォントサイズ調整

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -5,13 +5,13 @@ module NotificationsHelper
     
     case notification.action
     when "comment_to_post"
-      link_to("<strong>#{notification.sender.name[0, 10]}</strong> があなたの <strong>投稿</strong> - <strong>#{notifiable_name(notification)}</strong> にコメントしました".html_safe, 
+      link_to("<span>#{notification.sender.name[0, 10]}</span> があなたの <span>投稿</span> - <span>#{notifiable_name(notification)}</span> にコメントしました".html_safe, 
       post_path(notification.notifiable.post), data: { turbo: false }, class: "link-secondary link-offset-2 link-offset-2-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover")
     when "reply_to_comment"
-      link_to("<strong>#{notification.sender.name[0, 10]}</strong> があなたの <strong>コメント</strong> - <strong>#{notifiable_name(notification)}</strong> に返信しました".html_safe, 
+      link_to("<span>#{notification.sender.name[0, 10]}</span> があなたの <span>コメント</span> - <span>#{notifiable_name(notification)}</span> に返信しました".html_safe, 
       comment_comment_replies_path(notification.notifiable.comment), data: { turbo: false }, class: "link-secondary link-offset-2 link-offset-2-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover")
     when "reply_to_comment_reply"
-      link_to("<strong>#{notification.sender.name[0, 10]}</strong> があなたの <strong>コメント返信</strong> - <strong>#{notifiable_name(notification)}</strong> に返信しました".html_safe, 
+      link_to("<span>#{notification.sender.name[0, 10]}</span> があなたの <span>コメント返信</span> - <span>#{notifiable_name(notification)}</span> に返信しました".html_safe, 
       comment_comment_replies_path(notification.notifiable.comment), data: { turbo: false }, class: "link-secondary link-offset-2 link-offset-2-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover")
     else
       "新規通知がありました"
@@ -23,11 +23,11 @@ module NotificationsHelper
     
     case notification.action
     when "comment_to_post"
-      "<strong>#{notification.sender.name[0, 10]}</strong> があなたの <strong>投稿</strong> - <strong>#{notifiable_name(notification)}</strong> にコメントしました".html_safe
+      "<span>#{notification.sender.name[0, 10]}</span> があなたの <span>投稿</span> - <span>#{notifiable_name(notification)}</span> にコメントしました".html_safe
     when "reply_to_comment"
-      "<strong>#{notification.sender.name[0, 10]}</strong> があなたの <strong>コメント</strong> - <strong>#{notifiable_name(notification)}</strong> に返信しました".html_safe
+      "<span>#{notification.sender.name[0, 10]}</span> があなたの <span>コメント</span> - <span>#{notifiable_name(notification)}</span> に返信しました".html_safe
     when "reply_to_comment_reply"
-      "<strong>#{notification.sender.name[0, 10]}</strong> があなたの <strong>コメント返信</strong> - <strong>#{notifiable_name(notification)}</strong> に返信しました".html_safe
+      "<span>#{notification.sender.name[0, 10]}</span> があなたの <span>コメント返信</span> - <span>#{notifiable_name(notification)}</span> に返信しました".html_safe
     else
       "新規通知がありました"
     end

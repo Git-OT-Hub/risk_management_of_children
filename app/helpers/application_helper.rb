f69bb914657b3bd6ac3563e58f2b5ef49a56c09c
Git-OT-Hub <nw7649sss@gmail.com>
2024-04-02 22:12:27 +0900

    Fix: Xシェア機能に関する修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,11 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'アプリ名',
-      title: '室内における子どもの安全対策を考えるサービス',
+      title: 'グッズを活用した室内における子どもの安全対策',
       reverse: true,
       charset: 'utf-8',
-      description: '診断結果に応じて、グッズを活用した室内の安全対策方法を紹介しています。',
-      keywords: '子ども,室内,安全対策,リスク管理,グッズ',
+      description: '診断結果に応じて、グッズを活用した室内における子どもの安全対策を紹介しています。',
+      keywords: '子ども,室内,安全対策,リスク管理,グッズ,診断',
       canonical: request.original_url,
       separator: '|',
       og: {
@@ -25,13 +25,13 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('test2_twitter.jpg'),
+        image: image_url('test_twitter.jpg'),
         local: 'ja-JP'
       },
       twitter: {
         card: 'summary_large_image',
         site: '@OhmuraTomo',
-        image: image_url('test2_twitter.jpg')
+        image: image_url('test_twitter.jpg')
       }
     }
   end

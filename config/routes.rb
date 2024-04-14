8498289d7603fef70b0f331270ed94b63cfc3fae
Git-OT-Hub <nw7649sss@gmail.com>
2024-04-14 18:29:22 +0900

    Fix: サイトマップの設定修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   get "oauth/callback", to: "oauths#callback"
   get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
 
+  get '/sitemap', to: redirect("https://s3-ap-northeast-1.amazonaws.com/#{ENV['S3_BUCKET_NAME']}/sitemaps/sitemap.xml.gz")
+
   resources :users, only: %i[new create]
   resources :posts, only: %i[index new create show edit update destroy] do
     member do
